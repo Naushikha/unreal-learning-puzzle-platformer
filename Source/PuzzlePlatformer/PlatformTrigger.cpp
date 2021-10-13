7bacca1173f7ac435bc884f14af02e48d0233466
Naushikha Jayawickrama <naushikha.jayawickrama@gmail.com>
2021-10-13 13:35:19 +0530

    Added overlap events into TriggerVolume of PlatformTrigger

--- Source/PuzzlePlatformer/PlatformTrigger.cpp
+++ Source/PuzzlePlatformer/PlatformTrigger.cpp
@@ -18,11 +18,23 @@ APlatformTrigger::APlatformTrigger()
 void APlatformTrigger::BeginPlay()
 {
 	Super::BeginPlay();
+	// If you put this in the constructor it won't work in the viewport, since the world is already loaded and that leaves the constructor code already run or something
+	TriggerVolume->OnComponentBeginOverlap.AddDynamic(this, &APlatformTrigger::OnOverlapBegin);
+	TriggerVolume->OnComponentEndOverlap.AddDynamic(this, &APlatformTrigger::OnOverlapEnd);
 }
 
 // Called every frame
 void APlatformTrigger::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+}
+
+// This wasn't available straight away > https://docs.unrealengine.com/4.26/en-US/ProgrammingAndScripting/ClassCreation/CodeOnly/ 
+void APlatformTrigger::OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) {
+	UE_LOG(LogTemp, Warning, TEXT("Activated"));
+}
+
+void APlatformTrigger::OnOverlapEnd(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex) {
+	UE_LOG(LogTemp, Warning, TEXT("Deactivated"));
 }
 
